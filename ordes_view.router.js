93453a19a7992dc653e4269858d2ba907b672b66
aland bakr <110042148+aland03@users.noreply.github.com>
2023-02-28 20:47:41 +0300

    Delete ordes_view.router.js

--- ordes_view.router.js
+++
@@ -1,7 +0,0 @@
-const controller = require('../controller/orders_view.controller')
-const express = require('express')
-const app = express()
-
-app.get('/get-all', controller.getAllOrdersView())
-
-module.exports = app
