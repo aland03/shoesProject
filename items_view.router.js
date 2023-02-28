ecc6a42eb66af380fa78d08357bf1c775f5cd892
aland bakr <110042148+aland03@users.noreply.github.com>
2023-02-28 20:46:48 +0300

    Delete items_view.router.js

--- items_view.router.js
+++
@@ -1,7 +0,0 @@
-const controller = require('../controller/items_view.controller')
-const express = require('express')
-const app = express()
-
-app.get('/get-all', controller.getAllItemsView())
-
-module.exports = app
